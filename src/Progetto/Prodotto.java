db448be01944e1f0fca6f5f2eac4b791f3528b20
Maria Pia Stella <mariapiastella6@gmail.com>
2025-03-06 13:54:51 +0100

    Aggiunta di un costruttore che inizializza nome e prezzo

--- src/Progetto/Prodotto.java
+++ src/Progetto/Prodotto.java
@@ -5,4 +5,9 @@ public abstract class Prodotto implements ConPrezzo {
     private String nome; 
     private double prezzo;
     
+    public Prodotto(String nome, double prezzo) {
+        this.nome = nome; 
+        this.prezzo = prezzo;
+    }
+    
 }
